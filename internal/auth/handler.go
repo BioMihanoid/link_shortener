007a3bed68d4fd741172c3548ee70e3f08d19b29
mikhail <pechnikovmichelle@gmail.com>
2025-03-02 16:18:06 +0300

    fix: repair work validate changing struct tag

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"web-app/pkg/req"
+	"web-app/pkg/res"
 )
 
 type AuthHandler struct {
@@ -22,11 +23,23 @@ func (h *AuthHandler) Login() http.HandlerFunc {
 			return
 		}
 		fmt.Println(result)
+		data := LoginResponse{
+			Token: "123",
+		}
+		res.JsonAnswer(w, 400, data)
 	}
 }
 
 func (h *AuthHandler) Register() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Register")
+		result, err := req.HandleBody[RegisterRequest](w, r)
+		if err != nil {
+			return
+		}
+		fmt.Println(result)
+		data := RegisterResponse{
+			Token: "123",
+		}
+		res.JsonAnswer(w, 400, data)
 	}
 }
--- internal/auth/payload.go
+++ internal/auth/payload.go
@@ -1,10 +1,20 @@
 package auth
 
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
 type LoginResponse struct {
 	Token string `json:"token"`
 }
 
-type LoginRequest struct {
+type RegisterRequest struct {
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required, min=8"`
+	Name     string `json:"name" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type RegisterResponse struct {
+	Token string `json:"token"`
 }
